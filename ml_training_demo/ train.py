04beb4aab43b9800c94d8eb88f27a88028ffafb4
hfz111 <80017295+hfz111@users.noreply.github.com>
2026-03-09 18:27:53 +0800

    Update  train.py

--- ml_training_demo/ train.py
+++ ml_training_demo/ train.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
-from dataset import RandomDataset
-from model import SimpleModel
+from ml_training_demo.dataset import RandomDataset
+from ml_training_demo.model import SimpleModel
 
 dataset = RandomDataset()
 loader = DataLoader(dataset, batch_size=16)
